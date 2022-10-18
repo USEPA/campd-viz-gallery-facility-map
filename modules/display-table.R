347405042e6d0656b1715612e5545f84b378b9cd
kstetsyuk <stetsyuk.karina@epa.gov>
2022-10-18 09:54:45 -0400

    uswds table class names

--- modules/display-table.R
+++ modules/display-table.R
@@ -1,11 +1,11 @@
 
 # generate html grid from data frame
-getHTML <- function (frames) {
-  innerhtml = '<table class="display-table">'
+getTableHTML <- function (frames) {
+  innerhtml = '<table class="display-table row-coloring">'
   header_html = '<tr class="display-table-row">'
   innerhtml = paste(innerhtml,header_html)
   for (col in colnames(frames)){
-    header_item <- '<th class="display-table-header">'
+    header_item <- '<th class="display-table-header padding-y-1 padding-x-1 border border-base-dark">'
     cell <- sprintf("<span col-name='%s'>%s</span>", 
                     col,col)
     header_item = paste(header_item, cell, '</th>')
@@ -15,8 +15,11 @@ getHTML <- function (frames) {
   rows_html <- paste(unlist(lapply(1:nrow(frames), function(row){
     row_html = '<tr class="display-table-row">'
     for (col in colnames(frames)){
-      row_html = paste(row_html,'<td class="display-table-cell">')
+      row_html = paste(row_html,'<td class="display-table-cell padding-y-1 padding-x-1 border border-base-dark">')
       cellVal <- frames[row,col]
+      if (is.na(cellVal)){
+        cellVal <- "Not Applicable"
+      }
       cell <- sprintf("<span cell-value='%s'>%s</span>",
                       cellVal,cellVal)
       row_html = paste(row_html,cell)
