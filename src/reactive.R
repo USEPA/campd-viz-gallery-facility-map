5a8405b8c8770e2f64faaf4fd8c973f2decd2458
kstetsyuk <stetsyuk.karina@epa.gov>
2022-10-18 09:56:09 -0400

    edit display table

--- src/reactive.R
+++ src/reactive.R
@@ -1,9 +1,11 @@
 ## global reactives
 
 programInfo <- reactiveValues(allPrograms = NULL,
+                              currentPrograms = NULL,
                               allCompliancePrograms = NULL,
                               currentCompliancePrograms = NULL,
-                              descriptionTable = NULL)
+                              currentComplianceProgramsDescriptionTable = NULL,
+                              retiredComplianceProgramsDescriptionTable = NULL)
 
 
 statesMdm <- reactiveVal(NULL)
@@ -14,21 +16,54 @@ unitData <- reactiveVal(NULL)
 # facility map data 
 programFacilityData <- reactiveVal(NULL)
 
+latestComplianceYear <- reactiveVal(NULL)
+
 
 load_global_vars <- function(){
   
-  descriptionTable <- currentCompliancePrograms[,c("programCode",
-                                                   "programDescription",
-                                                   "compParameterCode",
-                                                   "programGroupCode",
-                                                   "programGroupDescription")]
+  # Get all current programs
+  allPrograms <- read.csv(paste0(dataGitRawBase,"programTable.csv"))
+  currentPrograms <- allPrograms[(allPrograms$retiredIndicator==FALSE),]
+  
+  # get retired compliace programs
+  retiredCompliancePrograms <- allPrograms[(allPrograms$retiredIndicator==TRUE &
+                                              allPrograms$allowanceUIFilter == TRUE),]
+  
+  # Get all allowance programs 
+  #compliancePrograms <- read.csv(paste0(dataGitRawBase,"allowanceProgramTable.csv"))
+  allCompliancePrograms <- allPrograms[allPrograms$allowanceUIFilter == TRUE,]
+  compliancePrograms <- allCompliancePrograms
+  
+  for (i in 1:nrow(allCompliancePrograms)){
+    if (!is.na(allCompliancePrograms$complianceYears[i])){
+      allCompliancePrograms$complianceYears[i] <- list(c(as.integer(unlist(strsplit(compliancePrograms$complianceYears[i], ",")))))
+    }
+    if (!is.na(allCompliancePrograms$emissionYears[i])){
+      allCompliancePrograms$emissionYears[i] <- list(c(as.integer(unlist(strsplit(compliancePrograms$emissionYears[i], ",")))))
+    }
+  }
+  
+  currentCompliancePrograms <- allCompliancePrograms[allCompliancePrograms$retiredIndicator == FALSE,]
+  
+  latestComplianceYear <<- min(na.omit(unlist(lapply(currentCompliancePrograms$programCode, function(program){
+    max(unlist(currentCompliancePrograms$complianceYears[currentCompliancePrograms$programCode == program]))
+  }))))
+  
+  currentComplianceProgramsDescriptionTable <- currentCompliancePrograms[,c("programCode",
+                                                   "programShorthandDescription",
+                                                   "compParameterCode")]
+  
+  retiredComplianceProgramsDescriptionTable <- retiredCompliancePrograms[,c("programCode",
+                                                                            "programShorthandDescription",
+                                                                            "compParameterCode")]
   
   
-  names(descriptionTable) <- c("Program Code",
-                               "Full Program Name",
-                               "Pollutant",
-                               "Program Group",
-                               "Program Group Description")
+  names(currentComplianceProgramsDescriptionTable) <- c("Program Code",
+                                                        "Full Program Name",
+                                                        "Pollutant")
+  names(retiredComplianceProgramsDescriptionTable) <- c("Program Code",
+                                                        "Full Program Name",
+                                                        "Pollutant")
   
   
   # Storing states 
@@ -36,17 +71,20 @@ load_global_vars <- function(){
   res = GET(url)
   states <- fromJSON(rawToChar(res$content))
   
-  # Unit data
-  unitData <<- read.csv(file = paste0(dataGitRawBase,"/data/unitData.csv"))
-  
   ####### assigning global values #######
   programInfo$allPrograms <<- allPrograms
+  programInfo$currentPrograms <<- currentPrograms
   programInfo$allCompliancePrograms <<- allCompliancePrograms
   programInfo$currentCompliancePrograms <<- currentCompliancePrograms
-  programInfo$descriptionTable <<- descriptionTable
+  programInfo$currentComplianceProgramsDescriptionTable <<- currentComplianceProgramsDescriptionTable
+  programInfo$retiredComplianceProgramsDescriptionTable <<- retiredComplianceProgramsDescriptionTable
   
   statesMdm <<- states
   
-  programFacilityData <<- read.csv(file = paste0(dataGitRawBase,"/data/facilityLocationData.csv"))
-  
+}
+
+load_map_data <- function(){
+  # Unit data
+  unitData <<- read.csv(file = paste0(dataGitRawBase,"unitData.csv"))
+  programFacilityData <<- read.csv(file = paste0(dataGitRawBase,"facilityLocationData.csv"))
 }
